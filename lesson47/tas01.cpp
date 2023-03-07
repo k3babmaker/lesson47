b142a8094ff233c18419ce47697ca385348cdf01
Kostya Kozlovsky <monodygames@gmail.com>
2023-03-07 18:27:54 +0300

    Change.cpp

--- lesson47/tas01.cpp
+++ lesson47/tas01.cpp
@@ -1,3 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
 int main() {
+
+	int a;
+	int b;
+
+	cout << "input a: ";
+	cin >> a;
+
+	cout << "input b: ";
+	cin >> b;
+
+	cout << a << " + " << b << " = " << a + b << endl;
+
 	return 0;
 }
